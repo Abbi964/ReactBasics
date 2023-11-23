81816ce5f7fe6cd8593a6a3a5a8982077e1eadb1
abbi964 <abhinavthapliyal964@gmail.com>
2023-11-23 14:24:17 +0530

    Adding Event Listeners

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,11 +1,21 @@
 import './ExpenseDetails.css'
 
 function ExpenseDetails(props) {
+  function clickHandler(){
+    console.log('Clicked!!!!')
+  }
+
+  function deletionHandeler(e){
+    e.target.parentElement.parentElement.remove()
+  }
+
   return (
     <div className='expense-details'>
       <h2>{props.title}</h2>
       <div className="expense-item__description">{props.location}</div>
       <div className="expense-item__price">${props.amount}</div>
+      <button onClick={clickHandler}>Change Title</button>
+      <button onClick={deletionHandeler}>DELETE</button>
     </div>
   );
 }
