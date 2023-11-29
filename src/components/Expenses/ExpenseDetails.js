0b8f32221e55533c3c6ea44baa95f8e92bf425d6
abbi964 <abhinavthapliyal964@gmail.com>
2023-11-29 11:52:28 +0530

    React States

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,19 +1,30 @@
+import React, { useState } from 'react';
 import './ExpenseDetails.css'
 
 function ExpenseDetails(props) {
+
+  const [title , setTitle] = useState(props.title);
+  const [amount, setAmount] = useState(props.amount)
+
   function clickHandler(){
-    console.log('Clicked!!!!')
+    setTitle('Updated!!')
+    console.log(title)
   }
 
   function deletionHandeler(e){
     e.target.parentElement.parentElement.remove()
   }
 
+  function expenseHandler (){
+    setAmount('100.00');
+  }
+
   return (
     <div className='expense-details'>
-      <h2>{props.title}</h2>
+      <h2>{title}</h2>
       <div className="expense-item__description">{props.location}</div>
-      <div className="expense-item__price">${props.amount}</div>
+      <button onClick={expenseHandler}>Change Expense</button>
+      <div className="expense-item__price">${amount}</div>
       <button onClick={clickHandler}>Change Title</button>
       <button onClick={deletionHandeler}>DELETE</button>
     </div>
