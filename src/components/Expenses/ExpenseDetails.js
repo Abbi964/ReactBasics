5ed887a646a685245ac34d689b46e3c30b6fc46c
abbi964 <abhinavthapliyal964@gmail.com>
2024-01-02 20:27:03 +0530

    making Expense detatils a stateless (dumb) component

--- src/components/Expenses/ExpenseDetails.js
+++ src/components/Expenses/ExpenseDetails.js
@@ -1,31 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './ExpenseDetails.css'
 
 function ExpenseDetails(props) {
-
-  const [title , setTitle] = useState(props.title);
-  const [amount, setAmount] = useState(props.amount)
-
-  function clickHandler(){
-    setTitle('Updated!!')
-    console.log(title)
-  }
 
   function deletionHandeler(e){
     e.target.parentElement.parentElement.remove()
   }
 
-  function expenseHandler (){
-    setAmount('100.00');
-  }
-
   return (
     <div className='expense-details'>
-      <h2>{title}</h2>
+      <h2>{props.title}</h2>
       <div className="expense-item__description">{props.location}</div>
-      <button onClick={expenseHandler}>Change Expense</button>
-      <div className="expense-item__price">${amount}</div>
-      <button onClick={clickHandler}>Change Title</button>
+      <div className="expense-item__price">${props.amount}</div>
       <button onClick={deletionHandeler}>DELETE</button>
     </div>
   );
