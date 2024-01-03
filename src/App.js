6ae0ef82547d0f4a273f759600b486311aa43a8a
abbi964 <abhinavthapliyal964@gmail.com>
2024-01-03 13:37:58 +0530

    Implementing the filter feature

--- src/App.js
+++ src/App.js
@@ -10,34 +10,35 @@ function App() {
       id: "e1",
       title: "Toilet Paper",
       amount: 94.12,
-      date: new Date(2020, 7, 14),
+      date: new Date(2022, 7, 14),
       location: "Mumbai",
     },
     {
       id: "e2",
       title: "New TV",
       amount: 799.49,
-      date: new Date(2021, 2, 12),
+      date: new Date(2023, 2, 12),
       location: "Chennai",
     },
     {
       id: "e3",
       title: "Car Insurance",
       amount: 294.67,
-      date: new Date(2021, 2, 28),
+      date: new Date(2023, 2, 28),
       location: "Delhi",
     },
     {
       id: "e4",
       title: "New Desk (Wooden)",
       amount: 450,
-      date: new Date(2021, 5, 12),
+      date: new Date(2024, 5, 12),
       location: "Lucknow",
     },
   ];
 
-  // using satate for expenses
+  // using satate for expenses and year selected
   const [currentExpenses , setExpenses] = useState(expenses)
+  const [yearSelected, setYear] = useState('2024')
 
   let addExpenseHandler = (expense) =>{
     setExpenses((current)=>{
@@ -46,11 +47,23 @@ function App() {
     console.log(currentExpenses)
   }
 
+  let yearChangeHandeler = (e)=>{
+    setYear(e.target.value);
+  }
+
   return (
     <div>
       <NewExpense onAddExpence = {addExpenseHandler}/>
+      <Card className='expense-item'>
+        <h2>Filter By Year</h2> 
+        <select onChange={yearChangeHandeler} id='year'>
+          <option value='2024'>2024</option>
+          <option value='2023'>2023</option>
+          <option value='2022'>2022</option>
+        </select>
+      </Card>
       <Card className = 'expenses'>
-        {currentExpenses.map((exp) => (
+        {currentExpenses.filter((exp)=>exp.date.getFullYear() === +yearSelected).map((exp) => (
           <ExpenseItem
             key={exp.id}
             title={exp.title}
